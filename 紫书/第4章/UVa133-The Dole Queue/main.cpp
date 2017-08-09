8027b3e463bee499f7be6918bc0bd33a55ccbf94
Unknown <email@xluos.com>
2017-08-09 23:01:37 +0800

    日常

--- 紫书/第4章/UVa133-The Dole Queue/main.cpp
+++ 紫书/第4章/UVa133-The Dole Queue/main.cpp
@@ -7,36 +7,36 @@ int main()
     int n,m,k,ren[30]={0};
     while(~scanf("%d %d %d",&n,&k,&m),k+m+n)
     {
-        int count_k=0,count_m=0,flag=0;
+        int count_k=0,count_m=0,flag=n,i=1,j=n;
         for(int i=1;i<=n;i++) ren[i]=i;
-        for(int i=1;;i++)
+        while(n)
         {
-            if(i>n)i%=n;
-            if(ren[i])
+            for(count_k=0;;i++)
             {
-                count_k++;
-                count_m++;
+                if(i>flag) i%=flag;
+                if(ren[i]) count_k++;
+                if(count_k==k) break;
             }
-            if(count_k==k)
+            for(count_m=0;;j--)
             {
-                count_k=0;
+                if(j<0) j=flag;
+                if(ren[j]) count_m++;
+                if(count_m==m) break;
+            }
+            if(i==j)
+            {
                 printf("%3d",ren[i]);
-                ren[i]=0;
                 n--;
-                flag++;
+
             }
-            if(count_m==m)
+            else
             {
-                count_m=0;
-                if(ren[i])
-                {
-                    printf("%3d",ren[i]);
-                    ren[i]=0;
-                    n--;
-                    flag++;
-                }
+                printf("%3d%3d",ren[i],ren[j]);
+                n-=2;
+
             }
-            if(flag&&n) {flag=0;printf(",");}
+            ren[i]=0;ren[j]=0;
+            if(n) printf(",");
         }
     }
     return 0;
