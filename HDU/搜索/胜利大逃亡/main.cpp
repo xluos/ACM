9f9b85abd69d1ddc56d0f1ee345fe757d4686363
xluos <email@xluos.com>
2017-10-03 20:21:02 +0800

    胜利大逃亡AC

--- HDU/搜索/胜利大逃亡/main.cpp
+++ HDU/搜索/胜利大逃亡/main.cpp
@@ -3,14 +3,14 @@
 using namespace std;
 const int N = 55;
 char Chart[N][N][N];
-int vis[N][N][N],n,m,c,qx,qy,qz,X,Y,Z,t;
+int vis[N][N][N],a,b,c,X,Y,Z,t;
 int dir[][3] = {0,0,1,0,0,-1,0,1,0,0,-1,0,1,0,0,-1,0,0};
 struct node{
     int x,y,z,step;
 };
 bool judge(int x,int y,int z)
 {
-    if(x<0||x>=c||y<0||y>=n||z<0||z>=m||vis[x][y][z]||Chart[x][y][z] == 1)
+    if(x<0||x>=a||y<0||y>=b||z<0||z>=c||vis[x][y][z]||Chart[x][y][z] == 1)
         return false;
     return true;
 }
@@ -26,16 +26,16 @@ int BFS(int x,int y,int z)
     {
         q = Q.front();
         Q.pop();
-        //cout<<q.x<<' '<<q.y<<' '<<q.z<<endl;
         for(int i=0;i<6;i++)
         {
             p.x = q.x + dir[i][0];
             p.y = q.y + dir[i][1];
             p.z = q.z + dir[i][2];
             p.step = q.step + 1;
+            if(X-p.x+Y-p.y+Z-p.z+p.step > t) continue;
             if(p.x == X&&p.y == Y&&p.z == Z)
             {
-                return p.step;
+                return p.step > t ? -1:p.step;
             }
             if(judge(p.x,p.y,p.z))
             {
@@ -52,24 +52,22 @@ int main()
     scanf("%d",&w);
     while(w--)
     {
-        scanf("%d %d %d %d",&c,&n,&m,&t);
+        scanf("%d %d %d %d",&a,&b,&c,&t);
         memset(vis,0,sizeof(vis));
         //memset(Chart,0,sizeof(vis));
-        for(int k = 0;k<c;k++)
+        for(int k = 0;k<a;k++)
         {
-            for(int i=0;i<n;i++)
+            for(int i=0;i<b;i++)
             {
-                for(int j=0;j<m;j++)
+                for(int j=0;j<c;j++)
                 {
                     scanf("%d",&Chart[k][i][j]);
                 }
             }
         }
-
-        Y = n - 1; Z = m -1; X = c - 1;
-        int step = BFS(0,0,0);
-        if(step <= t) printf("%d\n",step);
-        else printf("-1\n");
+        Y = b - 1; Z = c -1; X = a - 1;
+        if(Chart[X][Y][Z] == 1) printf("-1\n");
+        else printf("%d\n",BFS(0,0,0));
     }
     return 0;
 }
