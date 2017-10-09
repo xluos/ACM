03eba4269652ffa6ba9d8d1c49c789b6ba51e4f5
xluos <email@xluos.com>
2017-10-09 21:27:49 +0800

    最短路ｄｉｊ

--- HDU/最短路/畅通工程续/main.cpp
+++ HDU/最短路/畅通工程续/main.cpp
@@ -8,48 +8,6 @@ const int N=210;
 int n,m,s,t;
 int Chara[N][N],dis[N],vis[N];
 
-
-int main()
-{
-    //freopen("/home/xluos/Desktop/input.txt","r",stdin);
-    while(~scanf("%d %d",&m,&n))
-    {
-        //cout<<"n,m:"<<n<<' '<<m<<endl;
-        int u,v,w;
-        memset(Chara,0x3f,sizeof(Chara));
-        for(int i=0; i<n; i++)
-        {
-            scanf("%d %d %d",&u,&v,&w);
-            if(w < Chara[u][v])
-                Chara[u][v] = Chara[v][u] = w;//,cout<<"u,v,w: "<<u<<' '<<v<<' '<<w<<endl;
-        }
-        scanf("%d %d",&s,&t);
-        //cout<<"s:t=="<<s<<' '<<t<<endl;
-        Dijkstra(s);
-        printf("%d\n",dis[t]==INF?-1:dis[t]);
-    }
-    return 0;
-}
-
-
-
-
-
-
-
-
-/**
-
-#include<bits/stdc++.h>
-
-using namespace std;
-
-const int INF=0x3f3f3f3f;
-const int N=210;
-
-int n,m,s,t;
-int Chara[N][N],dis[N],vis[N];
-
 void Dijkstra(int src)
 {
     for(int i=0; i<m; i++)
@@ -103,4 +61,3 @@ int main()
     }
     return 0;
 }
-*/
