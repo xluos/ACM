9dbf205761ee59538d7619de92b73e3ba14ef578
xluos <email@xluos.com>
2017-10-19 10:20:14 +0800

    刷题更新

--- HDU/Debug/main.cpp
+++ HDU/Debug/main.cpp
@@ -1,35 +1,77 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+const int N = 1004;
+int n,m;
+double Chara[N][N],dis[N];
+int vis[N];
+double C[N][N];
+void Dijkstra(int src)
+{
+    for(int i=0;i<n;i++)
+    {
+        dis[i] = Chara[src][i];
+        vis[i] = 0;
+    }
+    vis[src] = 1;
+    for(int j=0;j<n;j++)
+    {
+        double ans = 0;
+        int k;
+        for(int i=0;i<n;i++)
+        {
+            if(!vis[i]&&dis[i]>ans)
+            {
+                ans = dis[i];
+                k = i;
+            }
+        }
+        vis[k] = 1;
+        if(ans == 0) break;
+        for(int i=0;i<n;i++)
+        {
+            if(!vis[i]&&dis[i] < dis[k]*Chara[k][i])
+                dis[i] = dis[k]*Chara[k][i];
+        }
+    }
+}
 
 int main()
 {
-    int w[200009],l[200009];
-    int n;
+
     while(~scanf("%d",&n))
     {
+        memset(C,0x3f,sizeof(C));
         for(int i=0;i<n;i++)
         {
-            scanf("%d %d",&w[i],&l[i]);
-            w[i+n] = w[i];
-            l[i+n] = w[i];
-        }
-        int sum=0,t=0,maxx=0;
-        for(int i=0;i<2*n-1;i++)
-        {
-            sum += w[i];
-            sum -= l[i];
-            t++;
-            if(sum<0)
+            for(int j=0;j<n;j++)
             {
-                sum=0;
-                t=0;
+                double ans;
+                scanf("%lf",&ans);
+                if(C[i][j] < ans)
+                    C[i][j] = ans;
             }
-            if(maxx<t) maxx = t;
-            //cout<<"sum=="<<sum<<" t=="<<t<<endl;
         }
-        if(maxx > n)maxx = n;
-        cout<<maxx<<endl;
+        scanf("%d",&m);
+        while(m--)
+        {
+            for(int i=0;i<n;i++)
+            {
+                for(int j=0;j<n;j++)
+                {
+                    Chara[i][j] = C[i][j];
+                }
+            }
+            int a,b;
+            scanf("%d %d",&a,&b);
+            Dijkstra(a-1);
+            if(fabs(dis[b-1])<0.00001)
+                printf("What a pity!\n");
+            else
+                printf("%.3lf\n",dis[b-1]);
+        }
+
+
     }
     return 0;
 }
--- HDU/最短路/find the safest road/main.cpp
+++ HDU/最短路/find the safest road/main.cpp
@@ -1,9 +1,68 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
+const int N = 2004;
+int n,m;
+double Chara[N][N],dis[N];
+int vis[N];
+void Dijkstra(int src)
+{
+    for(int i=0;i<n;i++)
+    {
+        dis[i] = Chara[src][i];
+        vis[i] = 0;
+    }
+    vis[src] = 1;
+    dis[src] = 1;
+    for(int j=0;j<n;j++)
+    {
+        double ans = 0;
+        int k;
+        for(int i=0;i<n;i++)
+        {
+            if(!vis[i]&&dis[i]>ans)
+            {
+                ans = dis[i];
+                k = i;
+            }
+        }
+        vis[k] = 1;
+        if(ans == 0) break;
+        for(int i=0;i<n;i++)
+        {
+            if(!vis[i]&&dis[i] < dis[k]*Chara[k][i])
+                dis[i] = dis[k]*Chara[k][i];
+        }
+    }
+}
 
 int main()
 {
-    cout << "Hello world!" << endl;
+
+    while(~scanf("%d",&n))
+    {
+        if(n>1001) while(1);
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                double ans;
+                scanf("%lf",&Chara[i][j]);
+            }
+        }
+        scanf("%d",&m);
+        while(m--)
+        {
+            int a,b;
+            scanf("%d %d",&a,&b);
+            Dijkstra(a-1);
+            if(dis[b-1]==0)
+                printf("What a pity!\n");
+            else
+                printf("%.3lf\n",dis[b-1]);
+        }
+
+
+    }
     return 0;
 }
