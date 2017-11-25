d24a39c8ed0d3f86a8a77d670e51fc6ac8819356
xluos <email@xluos.com>
2017-11-25 23:41:14 +0800

    更新一个坑题

--- HDU/最短路/HDU Today/main.cpp
+++ HDU/最短路/HDU Today/main.cpp
@@ -1,28 +1,78 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-const int MAX = 6;
-int num[10],sum=0;
-void s(int n)
+const int INF = 0x3f3f3f3f;
+const int MAX = 155;
+int Chara[MAX][MAX],vis[MAX],dis[MAX];
+int n;
+map<string,int>mp;
+void Dijkstra(int sre,int sum)
 {
-    if(n >= MAX)
+    memset(vis,0,sizeof(vis));
+    for(int i=1;i < sum;i++)
     {
-        for(int i=0;i<MAX;i++)
-        {
-            printf("%d ",num[i]);
-        }
-        sum++;
-        printf("\n");
+        dis[i] = Chara[sre][i];
     }
-    for(int i=1;i<=6;i++)
+    dis[sre] = 0;
+    vis[sre] = 1;
+    for(int i=1;i<sum;i++)
     {
-        num[n] = i;
-        s(n+1);
+        int k,minn = INF;
+        for(int j=1;j<sum;j++)
+        {
+            if(!vis[j] && dis[j] < minn)
+            {
+                k = j;
+                minn = dis[j];
+            }
+        }
+        if(minn == INF) break;
+        vis[k] = 1;
+        for(int j=1;j < sum;j++)
+        {
+            if(!vis[j] && dis[j] > dis[k] + Chara[k][j])
+            {
+                dis[j] = dis[k] + Chara[k][j];
+            }
+        }
     }
 }
 int main()
 {
-    s(0);
-    cout<<sum<<endl;
+    while(scanf("%d",&n),~n)
+    {
+        memset(Chara,0x3f,sizeof(Chara));
+        int t=1,w;
+        string place1,place2,S,T;
+        cin>>S>>T;
+        mp[S] = t++;
+        mp[T] = t++;
+        for(int i=0;i<n;i++)
+        {
+            cin>>place1>>place2>>w;
+            if(!mp[place1]) mp[place1] = t++;
+            if(!mp[place2]) mp[place2] = t++;
+            if(Chara[mp[place1]][mp[place2]] > w)
+            {
+                Chara[mp[place1]][mp[place2]] = w;
+                Chara[mp[place2]][mp[place1]] = w;
+            }
+        }
+        Dijkstra(mp[S],t);
+        printf("%d\n",dis[mp[T]]==INF?-1:dis[mp[T]]);
+        mp.clear();
+    }
     return 0;
 }
+/**
+测试数据 起点终点相同
+6
+xiasha xiasha
+xiasha station 60
+xiasha ShoppingCenterofHangZhou 30
+station westlake 20
+ShoppingCenterofHangZhou supermarket 10
+xiasha supermarket 50
+supermarket westlake 10
+
+*/
