229be8bd4981f5c7969aadb84f78d8a79bbab0e8
xluos <email@xluos.com>
2017-11-27 23:22:06 +0800

    更新

--- HDU/最短路/HDU2923Einbahnstrasse/main.cpp
+++ HDU/最短路/HDU2923Einbahnstrasse/main.cpp
@@ -6,48 +6,83 @@ const int MAX = 105;
 const int INF = 0x3f3f3f3f;
 
 int Chara[MAX][MAX],vis[MAX],dis[MAX];
+map<string,int>mp;
 
-void Dijstra(int sre)
+void Floyd()
 {
     for(int i=1;i<=N;i++)
     {
-        dis[i] = Chara[sre][i];
+        Chara[i][i] = 0;
     }
-    dis[sre] = 0;
-    vis[sre] = 1;
-    for(int i=1;i<=N;i++)
+    for(int k=1;k<=N;k++)
     {
-        int k,minn = INF;
-        for(int j = 1;j<=N;j++)
+        for(int i=1;i<=N;i++)
         {
-            if(!vis[j] && dis[j] < minn)
+            for(int j = 1;j<=N;j++)
             {
-                k = j;
-                minn = dis[j];
-            }
-        }
-        vis[k] = 1;
-        if(minn == INF) break;
-        for(int j=1;j<=N;j++)
-        {
-            if(!vis[j] && dis[j] > dis[k] + Chara[k][j])
-            {
-                dis[j] = dis[k] + Chara[k][j];
+                if(Chara[i][j] > Chara[i][k] + Chara[k][j])
+                {
+                    Chara[i][j] = Chara[i][k] + Chara[k][j];
+                }
             }
         }
     }
+//    for(int i=1;i<=N;i++)
+//        {
+//            for(int j = 1;j<=N;j++)
+//            {
+//                if(Chara[i][j] < INF)
+//                {
+//                    printf("%d --> %d  %d\n",i,j,Chara[i][j]);
+//                }
+//            }
+//        }
 }
 int main()
 {
-    string s,t[1005];
+    int k = 1;
+    string s,t[1005],u,v;
+    char left,right;
+    int w;
     while(scanf("%d %d %d",&N,&C,&R),N+C+R)
     {
-        cin>>s;cout<<s+"aaa"<<endl;
+        memset(Chara,INF,sizeof(Chara));
+        mp.clear();
+        int COUNT = 1,sum = 0 ;
+        cin>>s;
+        mp[s] = COUNT++;
         for(int i=0;i<C;i++)
         {
             cin>>t[i];
-            cout<<t[i]<<endl;
+            if(!mp[t[i]])
+            {
+                mp[t[i]] = COUNT++;
+            }
         }
+        for(int i=0;i<R;i++)
+        {
+            cin>>u;
+            scanf(" %c-%d-%c ",&left,&w,&right);
+            cin>>v;
+            //cout<<u<<w<<v<<' '<<get_direction(w)<<' '<<get_weight(w)<<endl;
+            if(!mp[u]) mp[u] = COUNT++;
+            if(!mp[v]) mp[v] = COUNT++;
+            if(left == '<' && w < Chara[mp[v]][mp[u]])
+            {
+                Chara[mp[v]][mp[u]] = w;
+            }
+            if(right == '>' && w < Chara[mp[u]][mp[v]])
+            {
+                Chara[mp[u]][mp[v]] = w;
+            }
+        }
+        Floyd();
+        for(int i=0;i<C;i++)
+        {
+            sum += Chara[mp[s]][mp[t[i]]] + Chara[mp[t[i]]][mp[s]];
+            //cout<<i<<'.'<<dis[mp[t[i]]]<<' ' + t[i]<<endl;
+        }
+        printf("%d. %d\n",k++,sum);
     }
     return 0;
 }
